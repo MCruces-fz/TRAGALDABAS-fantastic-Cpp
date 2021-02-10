e8d1bae7a748175ea4487fb3c6de98b9e39d0428
MCruces-fz <mcsquared.fz@gmail.com>
2021-02-10 12:51:46 +0100

    Update do DST

--- DST/do_unpack.sh
+++ DST/do_unpack.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
+FILENAME=$1
+
 echo
-echo Analyzing file: $1
+echo Analyzing file: $FILENAME
 echo
 
 HLDDIR="/media/Datos2TB/tragaldabas/data/done/"
 OUTDIR="/media/Datos4TB/tragaldabas/data/Nov2020_dst/root/"
+LUPTAB="luptab.txt"
+CALPARS="/media/Datos2TB/damian/tragaldabas/2020DST/pars/2020_day_290_CalPars.txt"
 
-cat > unpack.swap <<"EOF"
+# cat > unpack.C <<EOF
+root -l -q <<EOF
 {
- Unpacker* u = new Unpacker($HLDDIR, "XXXXXXXXX", $OUTDIR, 10000000, "luptab.txt","/media/Datos2TB/damian/tragaldabas/2020DST/pars/2020_day_290_CalPars.txt");
+ Unpacker* u = new Unpacker("$HLDDIR", "$FILENAME", "$OUTDIR", 10000000, "$LUPTAB", "$CALPARS");
 }
 EOF
 
-ORDE=`echo s/XXXXXXXXX/$1/g`
+# ORDE=`echo s/XXXXXXXXX/$1/g`
 
-sed "$ORDE" unpack.swap > unpack.C
-rm unpack.swap
+# sed "$ORDE" unpack.swap > unpack.C
+# rm unpack.swap
 
-root -l -q unpack.C
+# root -l -q unpack.C
--- Monitoring/do_ascii.sh
+++ Monitoring/do_ascii.sh
@@ -4,33 +4,8 @@ COMMAND=$1
 FROMDATE=$2
 TODATE=$3
 
-# HLDDIR="/media/Datos2TB/tragaldabas/data/done/alberto/T01T02T03/20150107/done/"
-# HLDDIR="/home/mcruces/Documents/fptrucha_hits/root/"
-HLDDIR="/media/Datos2TB/tragaldabas/data/done/"
-
-
-function do_unpack() {
-    
-    FILENAME=$1
-    
-    echo
-    echo Analyzing file: $FILENAME
-    echo
-    
-    OUTDIR="/media/Datos4TB/tragaldabas/data/Nov2020_dst/root/"
-    LUPTAB="luptab.txt"
-    CALPARS="/media/Datos2TB/damian/tragaldabas/2020DST/pars/2020_day_290_CalPars.txt"
-    
-    # cat > unpack.C <<EOF
-    root -l -q <<EOF
-    {
-     Unpacker* u = new Unpacker("$HLDDIR", "$FILENAME", "$OUTDIR", 10000000, "$LUPTAB", "$CALPARS");
-    }
-EOF
-
-}
-
-
+# DIR="/media/Datos2TB/tragaldabas/data/done/alberto/T01T02T03/20150107/done/"
+DIR="/home/mcruces/Documents/fptrucha_hits/root/"
 
 function run() {
 
@@ -50,10 +25,9 @@ function run() {
 
     for (( date=$FROMDATE; date<=$TODATE; date++))
     do
-        for a in `find $HLDDIR -type f -name tr$date* -print`; 
+        for a in `find $DIR -type f -name tr$date* -print`; 
         do 
-            do_unpack() `basename $a`
-            # ./do_unpack.sh `basename $a`;
+            ./do_unpack.sh `basename $a`;
         done
     done
 }
