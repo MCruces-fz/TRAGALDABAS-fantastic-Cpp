6c4fb5a33e3416890a126d67d4e5ac42f014b38f
MCruces-fz <mcsquared.fz@gmail.com>
2021-02-11 11:34:18 +0100

    Deleted unnecesary data (unpack.C and drawHits.C)

--- Monitoring/unpack.C
+++
@@ -1,3 +0,0 @@
-{
-Unpacker* u = new Unpacker("/media/Datos2TB/tragaldabas/data/done/", "tr16001001130.hld", "/media/Datos4TB/people/mcruces/PNGDST/", 10000000, "/media/Datos2TB/tragaldabas/luptab/luptable_corr_20180423.txt", "/media/Datos2TB/mcruces/tragaldabas/2020DST/pars/2020_day_092_CalPars.txt");
-}
